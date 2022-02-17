f94357e09632056426d90c5227f346420926960c
Sachin Gandhi <sachingandhi@live.com>
2022-02-17 15:43:32 +0000

    seeded the database using a top movies API instead of using faker

--- Gemfile
+++ Gemfile
@@ -53,3 +53,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem "faker", "~> 2.19"
+
+gem "rest-client", "~> 2.1"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+
+Movie.delete_all
+i=0
+
+  url = 'http://tmdb.lewagon.com/movie/top_rated'
+  movie_serialized = URI.open(url).read
+  top_movies = JSON.parse(movie_serialized)
+    while i < 20
+    movie = Movie.new(title: top_movies["results"][i]["title"], overview: top_movies["results"][i]["overview"], rating: top_movies["results"][i]["vote_average"]/2)
+    i += 1
+    movie.save
+    puts movie.title
+    puts movie.overview
+    puts movie.rating
+    end
